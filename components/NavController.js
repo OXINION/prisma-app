82fc2e4c8063b9138cba2d3073d32f8936b4b81e
OXINION <brandonha00918@gmail.com>
2019-08-18 16:46:53 +0900

    #10.6 AuthContext part1

---
+++ components/NavController.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import { useIsLoggedIn } from "../AuthContext";
+
+export default () => {
+  const isLoggedIn = useIsLoggedIn();
+  console.log(isLoggedIn);
+  
+  return <Text>NavController</Text>;
+};
+
