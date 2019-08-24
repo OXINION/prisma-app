6272a15570a085c12e700c74ea543bab641b9e79
OXINION <brandonha00918@gmail.com>
2019-08-25 03:09:20 +0900

    #13.2 TabBar, Styles, Loader

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,11 +1,20 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import TabNavigation from "./TabNavigation";
 import PhotoNavigation from "./PhotoNavigation";
-import MessageNavigation from "./MessageNavigation"
+import MessageNavigation from "./MessageNavigation";
+import { stackStyles } from "./config";
 
 const MainNavigation = createStackNavigator(
   { TabNavigation, PhotoNavigation, MessageNavigation },
-  { headerMode: "none", mode: "modal" }
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        ...stackStyles
+      }
+    },
+    headerMode: "none",
+    mode: "modal"
+  }
 );
 
 export default createAppContainer(MainNavigation);
