204f192865ff81c4693d106824ad22d224f13069
OXINION <brandonha00918@gmail.com>
2019-08-24 16:04:00 +0900

    #12.5 Login part3

--- components/AuthButton.js
+++ components/AuthButton.js
@@ -5,13 +5,13 @@ import constants from "../constants";
 import { ActivityIndicator } from "react-native";
 
 const Touchable = styled.TouchableOpacity``;
+
 const Container = styled.View`
   background-color: ${props => props.theme.blueColor};
   padding: 10px;
   margin: 0px 50px;
   border-radius: 4px;
   width: ${constants.width / 2};
-  margin-top: 15px;
 `;
 
 const Text = styled.Text`
@@ -23,7 +23,7 @@ const Text = styled.Text`
 const AuthButton = ({ text, onPress, loading = false }) => (
   <Touchable disabled={loading} onPress={onPress}>
     <Container>
-      {loading ? <ActivityIndicator color={"white"}/> : <Text>{text}</Text>}
+      {loading ? <ActivityIndicator color={"white"} /> : <Text>{text}</Text>}
     </Container>
   </Touchable>
 );
--- components/AuthInput.js
+++ components/AuthInput.js
@@ -8,11 +8,11 @@ const Container = styled.View`
 `;
 
 const TextInput = styled.TextInput`
-  width:${constants.width / 2}
+  width: ${constants.width / 2};
   padding: 10px;
   background-color: ${props => props.theme.greyColor};
   border: 0.5px solid ${props => props.theme.darkGreyColor};
-  border-radius: 2px;
+  border-radius: 4px;
 `;
 
 const AuthInput = ({
@@ -22,7 +22,7 @@ const AuthInput = ({
   autoCapitalize = "none",
   returnKeyType = "done",
   onChange,
-  onEndEditing = () => null,
+  onSubmitEditing = () => null,
   autoCorrect = true
 }) => (
   <Container>
@@ -32,7 +32,7 @@ const AuthInput = ({
       returnKeyType={returnKeyType}
       placeholder={placeholder}
       autoCapitalize={autoCapitalize}
-      onEndEditing={onEndEditing}
+      onSubmitEditing={onSubmitEditing}
       autoCorrect={autoCorrect}
       value={value}
     />
@@ -53,7 +53,7 @@ AuthInput.propTypes = {
   autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"]),
   onChange: PropTypes.func.isRequired,
   returnKeyType: PropTypes.oneOf(["done", "go", "next", "search", "send"]),
-  onEndEditing: PropTypes.func,
+  onSubmitEditing: PropTypes.func,
   autoCorrect: PropTypes.bool
 };
 
