d4fa8989911167636f6dca91fe5f42106948e711
OXINION <brandonha00918@gmail.com>
2019-08-24 15:06:09 +0900

    #12.4 Login part2

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
 import { Alert } from "react-native";
+import { useMutation } from "react-apollo-hooks";
+import { LOG_IN } from "./AuthQueries";
 
 const View = styled.View`
   justify-content: center;
@@ -12,18 +14,33 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => {
+export default ({ navigation }) => {
   const emailInput = useInput("");
-  const handleLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const requestSecret = useMutation(LOG_IN, {
+    variables: {
+      email: emailInput.value
+    }
+  });
+  const handleLogin = async () => {
     const { value } = emailInput;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
     if (value === "") {
-      return Alert.alert("Email can not be empty");
+      return Alert.alert("Email can't be empty");
     } else if (!value.includes("@") || !value.includes(".")) {
-      return Alert.alert("Please insert the right email ");
+      return Alert.alert("Please write an email");
     } else if (!emailRegex.test(value)) {
-      return Alert.alert("That email is not valid");
+      return Alert.alert("That email is invalid");
+    }
+    try {
+      setLoading(true);
+      await requestSecret();
+      Alert.alert("Check your email");
+      navigation.navigate("Confirm");
+    } catch (e) {
+      Alert.alert("Can't log in now");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -37,7 +54,7 @@ export default () => {
           onEndEditing={handleLogin}
           autoCorrect={false}
         />
-        <AuthButton onPress={handleLogin} text="Log In" />
+        <AuthButton loading={loading} onPress={handleLogin} text="Log In" />
       </View>
     </TouchableWithoutFeedback>
   );
