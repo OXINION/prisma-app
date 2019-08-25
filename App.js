7101f4c7300e503c791975ad440ebd83243e856f
OXINION <brandonha00918@gmail.com>
2019-08-25 10:42:32 +0900

    true undefined :(

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import styles from "./styles";
 import NavController from "./components/NavController";
 import { AuthProvider } from "./AuthContext";
 
+AsyncStorage.clear();
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
@@ -32,6 +33,12 @@ export default function App() {
       });
       const client = new ApolloClient({
         cache,
+        request: async operation => {
+          const token = await AsyncStorage.getItem("jwt");
+          return operation.setContext({
+            headers: { Authorization: `Bearer ${token}` }
+          });
+        },
         ...apolloClientOptions
       });
       const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
