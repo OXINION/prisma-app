cd1a70941e2166829f7f56525cd27a747230fa8e
OXINION <brandonha00918@gmail.com>
2019-09-01 20:01:25 +0900

    Select Photos

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as Permissions from "expo-permissions";
-import { Image, ScrollView } from "react-native";
+import { Image, ScrollView, TouchableOpacity } from "react-native";
 import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components";
 import Loader from "../../components/Loader";
@@ -17,6 +17,9 @@ export default () => {
   const [hasPermission, setHasPermission] = useState(false);
   const [selected, setSelected] = useState();
   const [allPhotos, setAllPhotos] = useState();
+  const changeSelected = photo => {
+    setSelected(photo);
+  };
   const getPhotos = async () => {
     try {
       const { assets } = await MediaLibrary.getAssetsAsync();
@@ -56,16 +59,22 @@ export default () => {
                 style={{ width: constants.width, height: constants.height / 2 }}
                 source={{ uri: selected.uri }}
               />
-              <ScrollView contentContainerStyle={{flexDirection: "row"}}>
+              <ScrollView contentContainerStyle={{ flexDirection: "row" }}>
                 {allPhotos.map(photo => (
-                  <Image
+                  <TouchableOpacity
                     key={photo.id}
-                    source={{ uri: photo.uri }}
-                    style={{
-                      width: constants.width / 3,
-                      height: constants.height / 6
-                    }}
-                  />
+                    onPress={() => changeSelected(photo)}
+                  >
+                    <Image
+                      key={photo.id}
+                      source={{ uri: photo.uri }}
+                      style={{
+                        width: constants.width / 3,
+                        height: constants.height / 6,
+                        opacity: photo.id === selected.id ? 0.5 : 1
+                      }}
+                    />
+                  </TouchableOpacity>
                 ))}
               </ScrollView>
             </>
